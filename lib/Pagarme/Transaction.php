1893708002573eb69befebee60fb645d2259e28b
Marcelo Batista <marcelo@maxmilhas.com.br>
2014-09-18 17:57:30 -0300

    Fixed untracked files

--- lib/Pagarme/Transaction.php
+++ lib/Pagarme/Transaction.php
@@ -22,26 +22,25 @@ class PagarMe_Transaction extends PagarMe_TransactionCommon {
 	public function refund() 
 	{
 		try {
-			if($this->status == 'refunded') {
-				throw new PagarMe_Exception(null, array('message' => "Transaction already refunded!", 'type' => 'forbidden_action'));
-			}
 
-			if($this->status != 'paid') {
-				throw new PagarMe_Exception(null, array('message' => "Transaction needs to be paid to be refunded", 'type' => 'forbidden_action'));
-			}
-
+			// verifica se a transacao e refente a um cartao de creditos
 			if($this->payment_method != 'credit_card') {
+				// levanta excecao
 				throw new PagarMe_Exception(null, array('message' => "Boletos can't be refunded", 'type' => 'forbidden_action'));
+			}
+
+			// verifica se a transacao ja foi estornada
+			if($this->status == 'refunded') {
+				// levanta excecao
+				throw new PagarMe_Exception(null, array('message' => "Transaction already refunded!", 'type' => 'forbidden_action'));
 			}
 
 			$request = new PagarMe_Request(self::getUrl().'/'.$this->id . '/refund', 'POST');
 			$response = $request->run();
 			$this->refresh($response);
+
 		} catch(Exception $e) {
 			throw new PagarMe_Exception($e->getMessage());
 		}
-
 	}
 }
-
-?>
