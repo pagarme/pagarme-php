f53c6b98e25e6e64c476fc4b4a3a4a6a542743d8
Eduardo Nascimento <eduardo.nascimento@pagar.me>
2016-12-19 16:17:50 -0200

    Get BulkAnticipation limits

--- lib/BulkAnticipation/BulkAnticipationHandler.php
+++ lib/BulkAnticipation/BulkAnticipationHandler.php
@@ -5,6 +5,7 @@ namespace PagarMe\Sdk\BulkAnticipation;
 use PagarMe\Sdk\AbstractHandler;
 use PagarMe\Sdk\Recipient\Recipient;
 use PagarMe\Sdk\BulkAnticipation\Request\BulkAnticipationCreate;
+use PagarMe\Sdk\BulkAnticipation\Request\BulkAnticipationLimits;
 
 class BulkAnticipationHandler extends AbstractHandler
 {
@@ -14,6 +15,7 @@ class BulkAnticipationHandler extends AbstractHandler
     * @param  string $timeframe
     * @param  int $requestedAmount
     * @param  boolean $building
+    * @return BulkAnticipation
     */
     public function create(
         Recipient $recipient,
@@ -36,5 +38,22 @@ class BulkAnticipationHandler extends AbstractHandler
         $response->paymentDate = new \DateTime($response->paymentDate);
 
         return new BulkAnticipation(get_object_vars($response));
+    }
+
+    /**
+    * @param  Recipient $recipient
+    * @param  DateTime $paymentDate
+    * @param  string $timeframe
+    * @return  array
+    */
+    public function limits($recipient, $paymentDate, $timeframe)
+    {
+        $request = new BulkAnticipationLimits(
+            $recipient,
+            $paymentDate,
+            $timeframe
+        );
+
+        return $this->client->send($request);
     }
 }
---
+++ lib/BulkAnticipation/Request/BulkAnticipationLimits.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PagarMe\Sdk\BulkAnticipation\Request;
+
+use PagarMe\Sdk\Request;
+use PagarMe\Sdk\Recipient\Recipient;
+
+class BulkAnticipationLimits implements Request
+{
+    /**
+     * @var  Recipient
+     */
+    private $recipient;
+
+    /**
+     * @var  DateTime
+     */
+    private $paymentDate;
+
+    /**
+     * @var  string
+     */
+    private $timeframe;
+
+    /**
+     * @param  Recipient $recipient
+     * @param  DateTime $paymentDate
+     * @param  string $timeframe
+     */
+    public function __construct(
+        Recipient $recipient,
+        \DateTime $paymentDate,
+        $timeframe
+    ) {
+        $this->recipient       = $recipient;
+        $this->paymentDate     = $paymentDate;
+        $this->timeframe       = $timeframe;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPayload()
+    {
+        return [
+            'payment_date'     => $this->getFormatedPaymentDate(),
+            'timeframe'        => $this->timeframe
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return sprintf(
+            'recipients/%s/bulk_anticipations/limits',
+            $this->recipient->getId()
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return self::HTTP_GET;
+    }
+
+    private function getFormatedPaymentDate()
+    {
+        return substr(
+            $this->paymentDate->format('Uu'),
+            0,
+            13
+        );
+    }
+}
