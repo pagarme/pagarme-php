70b42f00ef944f2e57ea4fbb4f58c4bb28c10e81
Eduardo Nascimento <eduardo.nascimento@pagar.me>
2016-12-20 17:57:36 -0200

    List Payables

--- lib/Payable/PayableHandler.php
+++ lib/Payable/PayableHandler.php
@@ -4,6 +4,7 @@ namespace PagarMe\Sdk\Payable;
 
 use PagarMe\Sdk\AbstractHandler;
 use PagarMe\Sdk\Payable\Request\PayableGet;
+use PagarMe\Sdk\Payable\Request\PayableList;
 
 class PayableHandler extends AbstractHandler
 {
@@ -17,5 +18,24 @@ class PayableHandler extends AbstractHandler
         $response = $this->client->send($request);
 
         return new Payable(get_object_vars($response));
+    }
+
+    /**
+     * @param int $page
+     * @param int $count
+     */
+    public function getList($page = null, $count = null)
+    {
+        $request = new PayableList($page, $count);
+
+        $response = $this->client->send($request);
+
+        $payables = [];
+
+        foreach ($response as $payableData) {
+            $payables[] = new Payable(get_object_vars($payableData));
+        }
+
+        return $payables;
     }
 }
