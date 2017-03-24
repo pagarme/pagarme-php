3a4314bb898febc278c988bbd0d1fdfcce8d0d5b
Filipe Garcia <epilif09@gmail.com>
2017-03-24 14:46:46 -0300

    Dropped support for php 5.4 (guzzle 6 related). Added option to include the soft descriptor to the request body.

--- lib/Transaction/Request/CreditCardTransactionCreate.php
+++ lib/Transaction/Request/CreditCardTransactionCreate.php
@@ -29,7 +29,7 @@ class CreditCardTransactionCreate extends TransactionCreate
             $cardData['card_cvv'] = $this->transaction->getCardCvv();
         }
 
-        return array_merge($basicData, $cardData, $this->getCardInfo());
+        return array_merge($basicData, $cardData, $this->getCardInfo(), $this->getSoftDescriptor());
     }
 
     /**
@@ -44,5 +44,17 @@ class CreditCardTransactionCreate extends TransactionCreate
         if (!is_null($this->transaction->getCardHash())) {
             return ['card_hash' => $this->transaction->getCardHash()];
         }
+    }
+	
+	/**
+     * @return array
+     */
+    private function getSoftDescriptor()
+    {
+        if (!is_null($this->transaction->getSoftDescriptor())) {
+            return ['soft_descriptor' => $this->transaction->getSoftDescriptor()];
+        }
+
+        return [];
     }
 }
