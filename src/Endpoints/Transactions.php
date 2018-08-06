213bcd4ceec912c115ff59632f13c9a66f1d170f
piiih <philipe.paixao@pagar.me>
2018-08-06 16:43:50 -0300

    transactions: add create function

--- src/Endpoints/Transactions.php
+++ src/Endpoints/Transactions.php
@@ -3,6 +3,8 @@
 namespace PagarMe\Endpoints;
 
 use PagarMe\Client;
+use PagarMe\Routes;
+use PagarMe\Endpoints\EndpointInterface;
 
 class Transactions
 {
@@ -12,10 +14,24 @@ class Transactions
     private $client;
 
     /**
-     * @param $client \Pagarme\Client
+     * @param \Pagarme\Client $client
      */
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * @param array $payload
+     *
+     * @return \ArrayObject
+     */
+    public function create(array $payload)
+    {
+        return $this->client->request(
+            EndpointInterface::POST,
+            Routes::transactions()->base(),
+            ['json' => $payload]
+        );
     }
 }
---
+++ tests/unit/TransactionsTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace PagarMe\Test;
+
+use PagarMe\Client;
+use PagarMe\Endpoints\Transactions;
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
+use PagarMe\Test\Mocks\TransactionMock;
+
+final class TransactionTest extends TestCase
+{
+    public function testTransactionCreate()
+    {
+        $container = [];
+        $history = Middleware::history($container);
+
+        $mock = new MockHandler([
+            new Response(200, [], TransactionMock::mock())
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $handler->push($history);
+
+        $client = new Client('apiKey', ['handler' => $handler]);
+
+        $response = $client->transactions()->create([
+            "amount" => 1000,
+            "card_number" => "4111111111111111",
+            "card_cvv" => "123",
+            "card_expiration_date" => "0922",
+            "card_holder_name" => "John Doe",
+            "payment_method" => "credit_card",
+            "customer" => [
+                "external_id" => "1",
+                "name" => "John Doe",
+                "type" => "individual",
+                "country" => "br",
+                "documents" => [[
+                    "type" => "cpf",
+                    "number" => "00000000000"
+                ]],
+                "phone_numbers" => ["+551199999999"],
+                "email" => "aardvark.silva@pagar.me"
+            ],
+            "billing" => [
+                "name" => "John Doe",
+                "address" => [
+                    "country" => "br",
+                    "street" => "Avenida Brigadeiro Faria Lima",
+                    "street_number" => "1811",
+                    "state" => "sp",
+                    "city" => "Sao Paulo",
+                    "neighborhood" => "Jardim Paulistano",
+                    "zipcode" => "01451001"
+                ]
+            ],
+            "items" => [
+                [
+                    "id" => "r123",
+                    "title" => "Red pill",
+                    "unit_price" => 10000,
+                    "quantity" => 1,
+                    "tangible" => true
+                ],
+                [
+                    "id" => "b123",
+                    "title" => "Blue pill",
+                    "unit_price" => 10000,
+                    "quantity" => 1,
+                    "tangible" => true
+                ]
+            ]
+        ]);
+
+        $requestMethod = $container[0]['request']->getMethod();
+
+        $this->assertEquals('POST', $requestMethod);
+        $this->assertEquals($response->getArrayCopy(), json_decode(TransactionMock::mock(), true));
+    }
+}
