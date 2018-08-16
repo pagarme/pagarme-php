93aafd6296b4bdf775cdfecac77d95ab554a92ce
piiih <philipe.paixao@pagar.me>
2018-08-16 18:00:58 -0300

    tests: add PagarMeTestCase
    This commit adds a file that have functions to help to write the tests
    boilerplate.

---
+++ tests/unit/PagarMeTestCase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PagarMe\Test;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+
+abstract class PagarMeTestCase extends TestCase
+{
+    public $container;
+    /**
+     * @param string $mockName
+     *
+     * @return string
+     */
+    protected static function jsonMock($mockName)
+    {
+        return file_get_contents("tests/unit/Mocks/$mockName.json");
+    }
+
+    /**
+     * @param array $container
+     *
+     * @return string
+     */
+    protected static function getRequestMethod($container)
+    {
+        return $container[0]['request']->getMethod();
+    }
+
+    /**
+     * @param array $container
+     *
+     * @return string
+     */
+    protected static function getRequestUri($container)
+    {
+        return $container[0]['request']->getUri()->getPath();
+    }
+
+    /**
+     * @param array @container
+     * @param GuzzleHttp\Handler\MockHandler $mock
+     *
+     * @return GuzzleHttp\HandlerStack
+     */
+    protected static function buildHandler(&$container, $mock)
+    {
+        $history = Middleware::history($container);
+
+        $handler = HandlerStack::create($mock);
+        $handler->push($history);
+
+        return $handler;
+    }
+}
